072f43ae7a741de608238b4ca6275aeaad9997a5
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-01-20 17:53:36 -0300

    Atualização método Sort: ordenando a lista

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+let btnOrdernarPreco = document.getElementById('btnOrdenarPorPreco');
+btnOrdernarPreco.addEventListener('click', ordernarLivrosPorPreco);
+
+function ordernarLivrosPorPreco() {
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
